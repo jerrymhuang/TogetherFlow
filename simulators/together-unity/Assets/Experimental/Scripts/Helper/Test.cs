3047c00cd8aa8b2f722c381645197d8216a337bd
Jerry <aca.jerryh@gmail.com>
2024-06-09 17:01:30 -0400

    Test reorientation

--- simulators/together-unity/Assets/Experimental/Scripts/Helper/Test.cs
+++ simulators/together-unity/Assets/Experimental/Scripts/Helper/Test.cs
@@ -1,12 +1,36 @@
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class Test : MonoBehaviour
 {
+    private GameObject[] beacons;
+    private int beaconId;
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Start()
     {
-        /* transform.forward = <cos(z), sin(z)> = <sin(x), cos(x)> */
-        Debug.Log(Time.deltaTime + " " + transform.forward);
+        transform.position = new Vector3(
+            Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f));
+
+        transform.rotation = Quaternion.Euler(
+            new Vector3(0f, Random.Range(-180f, 180f), 0f)
+        );
+
+        beacons = GameObject.FindGameObjectsWithTag("Beacon");
+        beaconId = Random.Range(0, beacons.Length);
+    }
+
+    void Update()
+    {
+        LookTowards(beacons[beaconId]);
+        Debug.Log(beacons[beaconId].name);
+        Debug.DrawRay(transform.position, transform.forward);
+    }
+
+    void LookTowards(GameObject beacon)
+    {
+        Vector3 relativePosition = beacon.transform.position - transform.position;
+        transform.forward = Vector3.RotateTowards(transform.forward, relativePosition, 0.01f, 0.01f);
     }
 }
