404d5fccb08b26bb01f8f02078b10b5078e0539f
Jerry <aca.jerryh@gmail.com>
2024-06-14 13:07:18 -0400

    Initial implementation of locomotive agent for testing

--- simulators/together-unity/Assets/Experimental/Scripts/LocomotiveAgent.cs
+++ simulators/together-unity/Assets/Experimental/Scripts/LocomotiveAgent.cs
@@ -16,15 +16,34 @@ public class LocomotiveAgent : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         WalkTo(beacon);
     }
 
 
-    void WalkTo(GameObject beacon)
+    /// <summary>
+    /// Implements the 2D Euler-Maruyama scheme for random walk with varying drift.
+    /// </summary>
+    /// <param name="beacon"></param>
+    void WalkTo(GameObject beacon, float baseDrift = 1f, float decay = 0.01f, float scale = 1f)
     {
-
+        Vector3 position = transform.position;
+        float distanceToBeacon = Distance2D(position, beacon.transform.position);
+        float drift = baseDrift * distanceToBeacon * decay;
+        float displacement = drift * Time.deltaTime;
+        position.x += displacement + scale * Mathf.Sqrt(Time.deltaTime) * RNG.Gaussian();
+        position.z += displacement + scale * Mathf.Sqrt(Time.deltaTime) * RNG.Gaussian();
+        transform.position = position;
     }
 
+
+    float Distance2D(Vector3 start, Vector3 end)
+    {
+        float d;
+        float x = start.x - end.x;
+        float z = start.z - end.z;
+        d = x * x + z * z;
+        return d;
+    }
 }
