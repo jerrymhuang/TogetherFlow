f43283ae7e5da7bcb39fe34a70099bd5bcc8ed4e
Jerry <aca.jerryh@gmail.com>
2025-01-16 09:42:16 -0500

    Add function for partial pooling prior

--- togetherflow/priors.py
+++ togetherflow/priors.py
@@ -8,7 +8,15 @@ def complete_pooling_prior():
     weight = np.random.beta(2, 5)
     radius = np.random.beta(2, 2) * 5.
     v = np.random.beta(2, 2) * 2.
-    return np.array([weight, radius, v])
+    return np.array([weight, radius, v], dtype=np.float32)
+
+
+@njit
+def partial_pooling_prior():
+    alpha_w = np.random.uniform(1, 5)
+    beta_w = np.random.uniform(1, 5)
+
+    return np.array([alpha_w, beta_w], dtype=np.float32)
 
 
 @njit
@@ -19,8 +27,3 @@ def complete_pooling_prior_with_noise():
     radius = np.random.beta(2, 2) * 5.
     v = np.random.beta(2, 2) * 2.
     return np.array([weight, radius, v, external_noise, internal_noise])
-
-
-@njit
-def meta_prior_fun():
-    raise NotImplementedError
