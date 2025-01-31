6fa32e56072d7431fc4cfa683a8aa58551167da7
Jerry <aca.jerryh@gmail.com>
2025-01-31 10:16:46 -0500

    Update

--- togetherflow/priors.py
+++ togetherflow/priors.py
@@ -12,11 +12,32 @@ def complete_pooling_prior():
 
 
 @njit
-def partial_pooling_prior():
+def hyperprior():
+
+    hyperprior = np.zeros(2, dtype=np.float32)
     alpha_w = np.random.uniform(1, 5)
     beta_w = np.random.uniform(1, 5)
 
-    return np.array([alpha_w, beta_w], dtype=np.float32)
+    hyperprior[0] = alpha_w
+    hyperprior[1] = beta_w
+
+    return hyperprior
+
+
+@njit
+def partial_pooling_prior(hyperprior, num_agents=12):
+    alpha_w, beta_w = hyperprior[0], hyperprior[1]
+
+    weights = np.random.beta(alpha_w, beta_w, size=(num_agents, 1))
+
+    global_prior = np.zeros((2, 1), dtype=np.float32)
+    radius = np.random.beta(2, 2) * 5.
+    v = np.random.beta(2, 2) * 2.
+
+    global_prior[0] = radius
+    global_prior[1] = v
+
+    return np.concatenate((weights, global_prior), axis=-1)
 
 
 @njit
