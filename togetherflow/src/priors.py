b80574f61fb229f0dce03a3fa4e4534b9e4d6aa0
jerrymhuang <aca.jerryh@gmail.com>
2025-10-30 18:31:23 -0400

    Add prior for internal noise

--- togetherflow/src/priors.py
+++ togetherflow/src/priors.py
@@ -5,8 +5,8 @@ from numba import njit
 @njit
 def complete_pooling_prior():
     weight = np.random.beta(2., 2.)
-    radius = np.random.gamma(5., 1.)
+    radius = np.random.lognormal(0, 0.5)
     # radius = np.random.lognormal(0., 0.25)
     v = np.random.beta(2., 2.) * 2.
-    #focus = np.random.beta(2, 5)
-    return np.array([weight, radius, v], dtype=np.float32)
+    focus = np.random.beta(2, 5)
+    return np.array([weight, radius, v, focus], dtype=np.float32)
